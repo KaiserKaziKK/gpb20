40a38c8ce47869da873d99f1b95547236c9efa68
Shifat Mahmud <shifatmahmud2002@gmail.com>
2024-01-20 21:37:19 +0600

    new file add from shifat

---
+++ python.py
@@ -0,0 +1,26 @@
+'''fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+
+
+
+colour = [ " red", "green", "black"]
+for x in colour:
+  print(x)
+  
+for x in "black":
+  print(x)
+  
+  
+colour = [ " red", "green", "black"]
+for x in colour:
+  print(x)
+  if x == "green":
+     break'''
+     
+colour = [ " red", "green", "black"]
+for x in colour:
+  if x == "green":
+     continue
+  print(x)
+  new line added
